a50cc119e6176f8b53aa43c137013600ae01221d
umavedha <uma11.vedhachalam@gmail.com>
2023-04-25 14:28:33 +0530

    4 and 5 completed

--- reduce.js
+++ reduce.js
@@ -14,9 +14,9 @@ console.log('------------------------------------------');
 
 let productPriceList=[{id:1,productName:'Table',price:3260},
                       {id:2,productName:'Chair',price:1060},
-                      {id:1,productName:'Sofa',price:4800},
-                      {id:1,productName:'TV Unit',price:8078},
-                      {id:1,productName:'Wardrobe',price:13260}
+                      {id:3,productName:'Sofa',price:4800},
+                      {id:4,productName:'TV Unit',price:8078},
+                      {id:5,productName:'Wardrobe',price:13260}
                     ];
 let totalCost= productPriceList.reduce((a,b)=>{
     return a + b.price
@@ -45,3 +45,33 @@ console.log('------------------------------------------');
 // 4. Write a function that takes an array of objects with a quantity and price property and 
 // returns the total cost of all the objects, considering the quantity of each object. 
 // Use the reduce method to accomplish this.
+
+let priceList=[{id:1,productName:'Pen',price:30,quantity:2},
+                      {id:2,productName:'Eraser',price:10,quantity:1},
+                      {id:3,productName:'Box',price:40,quantity:4},
+                      {id:4,productName:'Scale',price:10,quantity:2},
+                    ];
+
+let cost= priceList.reduce((a,b)=>{
+    return a + b.price*b.quantity
+},0)
+console.log('4.Cost of all objects:',cost)
+console.log('------------------------------------------');
+
+
+// 5. Write a function that takes an array of strings and returns an object where the keys are the strings, 
+// and the values are the number of times each string appears in the array. Use the reduce method to accomplish this.
+
+let arrString=['uma','maha','maha','uma','saravana','muthu','marudhu','marudhu']
+
+let occurrance=arrString.reduce((a,b)=>{
+   if(b in a){
+    a[b]=a[b]+1
+   }
+   else{
+    a[b]=1
+   }
+   return a
+},{})
+console.log('5.Number of times each string appears in the array:',occurrance)
+console.log('------------------------------------------');
